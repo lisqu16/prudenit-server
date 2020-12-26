0ff9533b5c5e46a4eb139ded68808b7139a34149
liskqu <56258709+lisqu16@users.noreply.github.com>
2020-12-26 12:45:08 +0100

    config, json header

--- main.go
+++ main.go
@@ -3,19 +3,32 @@ package main
 import (
 	"log"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"net/http"
+	"os"
 )
 
+var (
+	Port	string
+)
+
+// handlers
 func loginUser(w http.ResponseWriter, r *http.Request) {
-	log.Println("login")
+	w.Header().Set("Content-Type", "application/json; charset=utf8")
 }
 
 func registerUser(w http.ResponseWriter, r *http.Request) {
-	log.Println("register")
+	w.Header().Set("Content-Type", "application/json; charset=utf8")
 }
 
+// config, etc.
 func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Failed while loading config")
+	}
+	Port = os.Getenv("PORT")
 }
 
 func main() {
@@ -23,5 +36,5 @@ func main() {
 	// routes
 	r.HandleFunc("/user/login", loginUser).Methods("POST")
 	r.HandleFunc("/user/register", registerUser).Methods("POST")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":"+Port, r))
 }
